1a99805da0234290b8b2bbc9625e9b6ba53712e8
Sheikh, Haroon <haroon.sheikh@sky.uk>
2019-01-02 16:46:12 +0000

    extends checkstyle to include the test directories

--- requests-example/src/test/java/io/github/sitture/StepImplementation.java
+++ requests-example/src/test/java/io/github/sitture/StepImplementation.java
@@ -13,7 +13,7 @@ public class StepImplementation {
     private HashSet<Character> vowels;
 
     @Step("Vowels in English language are <vowelString>.")
-    public void setLanguageVowels(String vowelString) {
+    public void setLanguageVowels(final String vowelString) {
         vowels = new HashSet<>();
         for (char ch : vowelString.toCharArray()) {
             vowels.add(ch);
@@ -21,13 +21,13 @@ public class StepImplementation {
     }
 
     @Step("The word <word> has <expectedCount> vowels.")
-    public void verifyVowelsCountInWord(String word, int expectedCount) {
+    public void verifyVowelsCountInWord(final String word, final int expectedCount) {
         int actualCount = countVowels(word);
         assertThat(expectedCount).isEqualTo(actualCount);
     }
 
     @Step("Almost all words have vowels <wordsTable>")
-    public void verifyVowelsCountInMultipleWords(Table wordsTable) {
+    public void verifyVowelsCountInMultipleWords(final Table wordsTable) {
         for (TableRow row : wordsTable.getTableRows()) {
             String word = row.getCell("Word");
             int expectedCount = Integer.parseInt(row.getCell("Vowel Count"));
@@ -37,7 +37,7 @@ public class StepImplementation {
         }
     }
 
-    private int countVowels(String word) {
+    private int countVowels(final String word) {
         int count = 0;
         for (char ch : word.toCharArray()) {
             if (vowels.contains(ch)) {
