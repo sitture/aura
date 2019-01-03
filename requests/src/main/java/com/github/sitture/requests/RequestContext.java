162421850b26b9c7e1d6ce975b25aadf589855b7
Sheikh, Haroon <haroon.sheikh@sky.uk>
2019-01-03 19:47:33 +0000

    Updates the requestcontext

--- requests/src/main/java/com/github/sitture/requests/RequestContext.java
+++ requests/src/main/java/com/github/sitture/requests/RequestContext.java
@@ -3,8 +3,9 @@ package com.github.sitture.requests;
 public final class RequestContext {
 
     private RequestContext() {
-
     }
+
+    private static Request request;
 
     public static void setRequest(final Request request) {
         RequestContext.request = request;
@@ -13,7 +14,5 @@ public final class RequestContext {
     public static Request getRequest() {
         return request;
     }
-
-    private static Request request;
 
 }
