fa8a8cf29c83d8885045417d026ae27af93291ba
Sheikh, Haroon <haroon.sheikh@sky.uk>
2019-01-07 13:50:54 +0000

    Adds a step to update versions before testing

---
+++ .circleci/update_versions.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# check to make sure version parameter is provided
+if [[ -z "$1" ]]; then
+    echo "missing newVersion argument!" ;
+    exit 1 ;
+fi
+
+echo "Setting newVersion to '$1' ..."
+mvn -f ../ versions:set \
+    -DnewVersion=$1 \
+    -DgenerateBackupPoms=false \
+    -DprocessAllModules=true \
+    -DprocessDependencies=true
+echo "Setting archetype dependency newVersion to '$1' ..."
+mvn -f ../quickstart/src/main/resources/archetype-resources/ \
+    versions:set-property \
+    -Dproperty=gauge-requests.version \
+    -DnewVersion=$1 \
+    -DgenerateBackupPoms=false \
+    -DgroupId=com.sitture.github \
+    -DartifactId=gauge-requests-basic \
+    -Dversion=0.3.0
+mvn -f ../quickstart/src/test/resources/projects/basic/reference/ \
+    versions:set-property \
+    -Dproperty=gauge-requests.version \
+    -DnewVersion=$1 \
+    -DgenerateBackupPoms=false
