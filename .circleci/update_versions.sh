154e9c52b27b437c895e773b82a6f09dc0274ee4
Sheikh, Haroon <haroon.sheikh@sky.uk>
2019-01-08 20:01:23 +0000

    Updates the artifact id to reference

--- .circleci/update_versions.sh
+++ .circleci/update_versions.sh
@@ -19,7 +19,7 @@ mvn -f ./quickstart/src/main/resources/archetype-resources/ \
     -DnewVersion=$1 \
     -DgenerateBackupPoms=false \
     -DgroupId=com.sitture.github \
-    -DartifactId=gauge-requests-basic \
+    -DartifactId=gauge-requests-reference \
     -Dversion=$1
 mvn -f ./quickstart/src/test/resources/projects/basic/reference/ \
     versions:set-property \
