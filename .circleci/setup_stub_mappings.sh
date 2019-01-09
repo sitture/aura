5f410e77e32aa5bccfb4ef4a93eb82483ca7a53d
Sheikh, Haroon <haroon.sheikh@sky.uk>
2019-01-09 20:01:08 +0000

    Adds a script to setup stub mappings

---
+++ .circleci/setup_stub_mappings.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+stub_directory="stubapi"
+stub_api="http://localhost:8080/__admin/mappings/new"
+
+# check if stub Directory exists
+if [ ! -d "$stub_directory" ]; then
+  echo "$stub_directory directory not found!" ;
+  exit 1 ;
+fi
+
+mappings=$(find $stub_directory -name "*.json" -type f)
+
+for mapping in $mappings ; do
+    echo "$mapping"
+    curl -i -X POST --data @$mapping $stub_api
+done
